d4061fe07bc1ac7e826d10e2f4f414f6414603af
VullnetJ <v.jata24@gmail.com>
2021-08-29 17:39:20 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+
+import React from 'react';
 import './App.css';
+import { gql } from 'apollo-boost';
+import { useQuery } from '@apollo/react-hooks';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
+    const cityquery = gql`
+    {
+        cities { 
+            name, 
+            country{name}, 
+      }
+        jobs { 
+            company{name}, 
+            applyUrl, 
+        }
+    }`
+
+    const { loading, error, data } = useQuery(cityquery);
+
+    if (loading) // If still loading
+        return "Loading...";
+    if (error) // If something goes wrong 
+        return `Error ${error.message}`;
+
+
+    return (
+        <div className="App">
+            <table>
+                <tbody>
+                    {
+                        data.cities.map((city, index) =>
+                            <tr key={index}>
+                                <td>{city.name}</td>
+                                <td>{city.country.name}</td>
+                            </tr>
+                        )
+
+                    } {
+                        data.jobs.map((job, index) =>
+                            <tr key={index}>
+                                <td>{job.company.name}</td>
+                                <td><a href={job.applyUrl}>{job.applyUrl}</a></td>
+                            </tr>
+                        )
+                    }
+                </tbody>
+            </table>           
+        </div>
+    );
+};
 export default App;
